988b9ee53a6136fcd4c6c89fd2e65453d18bd595
Lieslot <rlaxodud1999@gmail.com>
2023-10-15 21:44:12 +0900

    [chore] ãã사소한 문êRangeException 오류문구 수정

--- src/main/java/baseball/exception/Number/RangeException.java
+++ src/main/java/baseball/exception/Number/RangeException.java
@@ -4,7 +4,7 @@ import static baseball.NumbersEnum.*;
 
 public class RangeException extends IllegalArgumentException{
 
-    public static final String ERROR_MESSAGE = String.format("숫자의 범위는 %d에서 %d 사이여야 합니다.", MIN_RANGE.showValue(), MAX_RANGE.showValue());
+    public static final String ERROR_MESSAGE = String.format("각 자리의 숫자의 범위는 %d에서 %d 사이여야 합니다.", MIN_RANGE.showValue(), MAX_RANGE.showValue());
 
     public RangeException() {
         super(ERROR_MESSAGE);
